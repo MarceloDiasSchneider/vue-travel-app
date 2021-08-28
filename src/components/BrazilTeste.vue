e520c29ce6d8eb586299aec52243993c8b5a3cdd
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-08-29 00:15:09 +0200

    Lazy load to the component BrazilTeste | Test a commit with more than  caracteres

---
+++ src/components/BrazilTeste.vue
@@ -0,0 +1,5 @@
+<template>
+  <div if>
+    <p>Ola Brazil!!</p>
+  </div>
+</template>
--- src/views/Brazil.vue
+++ src/views/Brazil.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="brazil">
     <h1>Brazil</h1>
+    <button @click="teste = !teste">Teste Brazil</button>
+    <div v-if="teste">
+      <Teste />
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+// import Teste from "@/components/BrazilTeste";
+
+export default {
+  // components: {
+  //   Teste,
+  // },
+  components: {
+    Teste: () =>
+      import(
+        /* webpackChunkName: "brazil-teste" */ "../components/BrazilTeste.vue"
+      ),
+  },
+  data() {
+    return {
+      teste: false,
+    };
+  },
+};
+</script>
