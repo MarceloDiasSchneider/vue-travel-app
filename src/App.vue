9e4590141d1b188ce1f59197e62f24da969871ba
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-08-30 17:57:51 +0200

    setting transition time

--- src/App.vue
+++ src/App.vue
@@ -22,13 +22,13 @@ export default {
 <style lang="css">
 .slide-enter-active,
 .slide-leave-active {
-  transition: opacity 1s, transform 0.3s;
+  transition: opacity 0.3s, transform 0.3s;
 }
 .slide-enter,
 .slide-leave-to {
   opacity: 0;
   /* Start the shake animation and make the animation last for 0.5 seconds */
-  animation: shake 1s;
+  animation: shake 0.3s;
 
   /* When the animation is finished, start again */
   animation-iteration-count: infinite;
@@ -38,35 +38,17 @@ export default {
   0% {
     transform: translate(1px, 1px) rotate(0deg);
   }
-  10% {
+  25% {
     transform: translate(-1px, -2px) rotate(-1deg);
   }
-  20% {
+  50% {
     transform: translate(-3px, 0px) rotate(1deg);
   }
-  30% {
+  75% {
     transform: translate(3px, 2px) rotate(0deg);
   }
-  40% {
-    transform: translate(1px, -1px) rotate(1deg);
-  }
-  50% {
-    transform: translate(-1px, 2px) rotate(-1deg);
-  }
-  60% {
-    transform: translate(-3px, 1px) rotate(0deg);
-  }
-  70% {
-    transform: translate(3px, 1px) rotate(-1deg);
-  }
-  80% {
-    transform: translate(-1px, -1px) rotate(1deg);
-  }
-  90% {
-    transform: translate(1px, 2px) rotate(0deg);
-  }
   100% {
-    transform: translate(1px, -2px) rotate(-1deg);
+    transform: translate(1px, -1px) rotate(1deg);
   }
 }
 </style>
