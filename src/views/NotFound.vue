227edc75a133480daf084c3aae8f1649eb2c0538
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-08-30 17:35:55 +0200

    Not Found view and navigation guards in component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Oops, We are sorry</h1>
+    <p>we couldn't find that page.</p>
+    <span>Erro 404! </span>
+    <router-link :to="{ name: 'Home' }">
+      <button>Home</button>
+    </router-link>
+  </div>
+</template>
